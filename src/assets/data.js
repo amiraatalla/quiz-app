7d8d0e56e6360641aecd27fa6508257475ad35f2
amiraatalla <amiraatalla63@gmail.com>
2024-08-01 23:12:08 +0200

    add dynamic questions

---
+++ src/assets/data.js
@@ -0,0 +1,45 @@
+const data = [
+    {
+      question: "What is the capital of France?",
+      option1: "Berlin",
+      option2: "Madrid",
+      option3: "Paris",
+      option4: "Lisbon",
+      ans: 3
+    },
+    {
+      question: "Who wrote 'To Kill a Mockingbird'?",
+      option1: "Harper Lee",
+      option2: "Jane Austen",
+      option3: "J.K. Rowling",
+      option4: "Mark Twain",
+      ans: 1
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      option1: "Earth",
+      option2: "Mars",
+      option3: "Jupiter",
+      option4: "Saturn",
+      ans: 3
+    },
+    {
+      question: "What is the chemical symbol for gold?",
+      option1: "Au",
+      option2: "Ag",
+      option3: "Fe",
+      option4: "Hg",
+      ans: 1
+    },
+    {
+      question: "Who painted the Mona Lisa?",
+      option1: "Vincent van Gogh",
+      option2: "Pablo Picasso",
+      option3: "Leonardo da Vinci",
+      option4: "Claude Monet",
+      ans: 3
+    }
+  ];
+  
+  export default data;
+  
